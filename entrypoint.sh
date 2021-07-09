5ac3e885db09b9503381f880d948df747a88008d
Marten Ringwelski <git@maringuu.de>
2021-07-09 12:56:47 +0200

    Copy over from fkie-cad/FACT_core docker-dev branch
    Copied from 0e63bb33f7598319cbd1956a16fffae785ffc99e

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+case "$1" in
+    "start")
+        shift 1
+        # This path could be configurable
+        # It is not really necessary to make it configurable since there is no
+        # usecase to change the database path inside the docker container
+        if [ -e /media/data/fact_wt_mongodb/REINITIALIZE_DB ]; then
+            python3 /opt/FACT_core/src/init_database.py && \
+                rm /media/data/fact_wt_mongodb/REINITIALIZE_DB
+        fi
+        exec /opt/FACT_core/start_all_installed_fact_components "$@"
+    ;;
+    "pull-containers")
+        # We can't to this in the Dockerfile, because the docker socket is not shared to there
+        exec /opt/FACT_core/src/install.py \
+            --backend-docker-images \
+            --frontend-docker-images
+    ;;
+    *)
+        printf "See https://github.com/fkie-cad/FACT_core/blob/master/docker/README.md for how to start this container\n"
+        exit 0
+esac
